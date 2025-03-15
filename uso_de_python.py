ed9d9c4f66d610a6d34d5f3c8836161123facd4f
alejotier94 <alejotier@gmail.com>
2025-03-14 19:18:01 -0500

    intruccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,32 @@
+#Autor: Alejandro Tierradentro
+#este es un comentario de una sola linea
+'''
+este es un comentario de 
+multiples
+lineas
+'''
+print("Estudiando python")
+
+#definicion de variables
+# no se deben dejar espacios enlas variables
+'''
+Regla
+Python es sencible y diferecnia mayusculas y miniculas
+no se puede usar palabras reservadas 
+no es conveniente usar numeros como nombre de variables
+'''
+x=50
+y=15
+print(x+y)
+print(type(x))
+#<class 'int'> : significa que x es un numero entero
+nombre="Alejandro"
+print(nombre)
+print(type(nombre))
+#<class 'str'> : significa que la variable nombre es un string ( una cadena de caracteres)
+print(type(x))
+x=4.8
+print(type(x))
+#<class 'float'> : signifaca que la variable es un flotante (decimal)
+
+print(x , nombre)
